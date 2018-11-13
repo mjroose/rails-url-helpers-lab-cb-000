e8a35da20c32fccfabc24806b94e54587950d46e
Michael Roose <mjroose@gmail.com>
2018-11-13 00:18:47 +0000

    automatically backed up by learn

--- db/migrate/20181113001831_add_column_to_student.rb
+++ db/migrate/20181113001831_add_column_to_student.rb
@@ -1,5 +1,5 @@
 class AddColumnToStudent < ActiveRecord::Migration
   def change
-    add_column :students, :active, :boolean
+    add_column :students, :active, :boolean, :default => false
   end
 end
